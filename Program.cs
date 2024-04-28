36844e36595942a76543aa475601b6090abc7be0
SwiftyKey <slipperyfish9@gmail.com>
2024-04-28 19:07:24 +0500

    Выполнил Задание 2

--- Program.cs
+++ Program.cs
@@ -1,19 +1,44 @@
-﻿string ProcessTheString(string? text)
+﻿public class StringHandler
 {
-	if (text == null) throw new ArgumentNullException(nameof(text));
-
-	if (text.Length == 0) return "";
-
-	if (text.Length % 2 == 0)
+	public string ProcessTheString(string? text)
 	{
-		var leftPart = text.Substring(0, text.Length / 2);
-		var rightPart = text.Substring(text.Length / 2);
-		return new string(leftPart.Reverse().ToArray()) + new string(rightPart.Reverse().ToArray());
+		if (text == null) throw new ArgumentNullException(nameof(text));
+
+		if (text.Length == 0 || !InputStringIsCorrect(text)) return "";
+
+		if (text.Length % 2 == 0)
+		{
+			var leftPart = text.Substring(0, text.Length / 2);
+			var rightPart = text.Substring(text.Length / 2);
+			return new string(leftPart.Reverse().ToArray()) + new string(rightPart.Reverse().ToArray());
+		}
+
+		return new string(text.Reverse().ToArray()) + text;
 	}
 
-	return new string(text.Reverse().ToArray()) + text;
+	private bool InputStringIsCorrect(string text)
+	{
+		var incorrectChars = text.Where(x => (x < 'a' || x > 'z')).ToList();
+
+		if (incorrectChars.Count == 0) return true;
+
+		OutputIncorrectChars(text, incorrectChars);
+		return false;
+	}
+
+	private void OutputIncorrectChars(string text, List<char> incorrectChars)
+	{
+		Console.WriteLine($"В введенной строек \'{text}\' имеются некорректные символы: {String.Join(", ", incorrectChars)}");
+	}
 }
 
-var inputText = Console.ReadLine();
-var processedString = ProcessTheString(inputText);
-Console.WriteLine(processedString);
+internal class MainClass
+{
+	static public void Main()
+	{
+		var stringHandler = new StringHandler();
+		var inputText = Console.ReadLine();
+		var processedString = stringHandler.ProcessTheString(inputText);
+		Console.WriteLine(processedString);
+	}
+}
