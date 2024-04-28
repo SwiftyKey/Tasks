001c8935ae41d26b28bba73610d636b1685d5f0c
SwiftyKey <slipperyfish9@gmail.com>
2024-04-28 17:16:16 +0500

    решил Задание 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string ProcessTheString(string? text)
+{
+	if (text == null) throw new ArgumentNullException(nameof(text));
+
+	if (text.Length == 0) return "";
+
+	if (text.Length % 2 == 0)
+	{
+		var leftPart = text.Substring(0, text.Length / 2);
+		var rightPart = text.Substring(text.Length / 2);
+		return new string(leftPart.Reverse().ToArray()) + new string(rightPart.Reverse().ToArray());
+	}
+
+	return new string(text.Reverse().ToArray()) + text;
+}
+
+var inputText = Console.ReadLine();
+var processedString = ProcessTheString(inputText);
+Console.WriteLine(processedString);
