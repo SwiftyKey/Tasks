4a8522745d9bd973c4afc8fa4e068b3a6618a693
SwiftyKey <slipperyfish9@gmail.com>
2024-04-28 21:51:16 +0500

    Выполнил Задание 3

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
-﻿public class StringHandler
+﻿using System.Text;
+
+public class StringHandler
 {
-	public string ProcessTheString(string? text)
+	public (string, Dictionary<char, int>?) ProcessTheString(string? text)
 	{
 		if (text == null) throw new ArgumentNullException(nameof(text));
 
-		if (text.Length == 0 || !InputStringIsCorrect(text)) return "";
+		if (text.Length == 0 || !InputStringIsCorrect(text)) return ("", null);
 
+		var processedText = new StringBuilder();
 		if (text.Length % 2 == 0)
 		{
 			var leftPart = text.Substring(0, text.Length / 2);
 			var rightPart = text.Substring(text.Length / 2);
-			return new string(leftPart.Reverse().ToArray()) + new string(rightPart.Reverse().ToArray());
-		}
 
-		return new string(text.Reverse().ToArray()) + text;
+			processedText.Append(new string(leftPart.Reverse().ToArray()));
+			processedText.Append(new string(rightPart.Reverse().ToArray()));
+		}
+		else
+		{
+			processedText.Append(new string(text.Reverse().ToArray()));
+			processedText.Append(text);
+		}
+		var processedTextToString = processedText.ToString();
+		return (processedTextToString, GetCountSymbols(processedTextToString));
 	}
 
 	private bool InputStringIsCorrect(string text)
@@ -30,15 +40,42 @@
 	{
 		Console.WriteLine($"В введенной строке \'{text}\' имеются некорректные символы: {String.Join(", ", incorrectChars)}");
 	}
+
+	private Dictionary<char, int> GetCountSymbols(string text)
+	{
+		var counter = new Dictionary<char, int>();
+		foreach (char c in text.ToHashSet())
+			counter[c] = text.Count(x => x == c);
+
+		return counter;
+	}
 }
 
 internal class MainClass
 {
-	static public void Main()
+	public static void Main()
 	{
 		var stringHandler = new StringHandler();
 		var inputText = Console.ReadLine();
-		var processedString = stringHandler.ProcessTheString(inputText);
-		Console.WriteLine(processedString);
+		var (processedString, counter) = stringHandler.ProcessTheString(inputText);
+
+		OutputProcessedString(processedString);
+		OutputCounter(counter);
+	}
+
+	private static void OutputProcessedString(string text)
+	{
+		if (text != "")
+			Console.WriteLine("Обработанная строка: " + text);
+	}
+
+	private static void OutputCounter(Dictionary<char, int>? counter)
+	{
+		if (counter != null)
+		{
+			Console.WriteLine("Количество вхождений каждого символа:");
+			foreach (char key in counter.Keys)
+				Console.WriteLine($"{key}: {counter[key]}");
+		}
 	}
 }
