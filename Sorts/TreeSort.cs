73c8989db40573a53d94f3efe4fce827c12a7ea3
Chernov Stepan <53141116+SwiftyKey@users.noreply.github.com>
2024-04-30 00:46:07 +0500

    Задание 5

---
+++ Sorts/TreeSort.cs
@@ -0,0 +1,50 @@
+﻿namespace Tasks.Sorts
+{
+	internal class BinaryTree<T> where T : IComparable
+	{
+		public T Data { get; set; }
+		public BinaryTree<T>? Left { get; set; } = null;
+		public BinaryTree<T>? Right { get; set; } = null;
+
+		public BinaryTree(T data) => Data = data;
+
+		public void Insert(BinaryTree<T> node, Comparer<T> comparer)
+		{
+			if (comparer.Compare(node.Data, Data) > 0)
+			{
+				if (Left == null) Left = node;
+				else Left.Insert(node, comparer);
+			}
+			else
+			{
+				if (Right == null) Right = node;
+				else Right.Insert(node, comparer);
+			}
+		}
+
+		public T[] Transform(List<T>? elements = null)
+		{
+			elements ??= new List<T>();
+
+			Left?.Transform(elements);
+			elements.Add(Data);
+			Right?.Transform(elements);
+
+			return elements.ToArray();
+		}
+	}
+
+	public class TreeSort<T> :ASort<T> where T : IComparable
+	{
+		public override Comparer<T> Comparer { get; set; } = new Comparer<T>();
+
+		public override T[] Sorting(T[] data)
+		{
+			var tree = new BinaryTree<T>(data[0]);
+			foreach(var item in data[1..])
+				tree.Insert(new BinaryTree<T>(item), Comparer);
+
+			return tree.Transform();
+		}
+	}
+}
---
+++ StringHandler.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Tasks
+{
+	public class StringHandler
+	{
+		public static string ProcessString(string? text)
+		{
+			if (text == null) throw new ArgumentNullException(nameof(text));
+
+			if (text.Length == 0 || !InputStringIsCorrect(text)) return "";
+
+			var processedText = new StringBuilder();
+			if (text.Length % 2 == 0)
+			{
+				var leftPart = text[..(text.Length / 2)];
+				var rightPart = text[(text.Length / 2)..];
+
+				processedText.Append(new string(leftPart.Reverse().ToArray()));
+				processedText.Append(new string(rightPart.Reverse().ToArray()));
+			}
+			else
+			{
+				processedText.Append(new string(text.Reverse().ToArray()));
+				processedText.Append(text);
+			}
+
+			return processedText.ToString();
+		}
+
+		public static Dictionary<char, int> GetCountSymbols(string text)
+		{
+			var counter = new Dictionary<char, int>();
+			foreach (char c in text.ToHashSet())
+				counter[c] = text.Count(x => x == c);
+
+			return counter;
+		}
+
+		public static string GetLargestSubstringStartsAndEndsWithVowel(string text)
+		{
+			Regex rg = new(@"[aeiouy][a-z]*[aeiouy]|[aeiouy]");
+			return rg.Match(text).Value;
+		}
+
+		private static bool InputStringIsCorrect(string text)
+		{
+			Regex rg = new(@"[^a-z]");
+			var incorrectChars = rg.Matches(text).Select(x => x.Value).ToList();
+
+			if (incorrectChars.Count == 0) return true;
+
+			OutputIncorrectChars(text, incorrectChars);
+			return false;
+		}
+
+		private static void OutputIncorrectChars(string text, List<string> incorrectChars)
+		{
+			Console.WriteLine($"В введенной строке \'{text}\' имеются некорректные символы: {String.Join(", ", incorrectChars)}");
+		}
+	}
+}
